14efdd6c5a1621ac0e611e12a4b88104ff691e32
Daniela <nam991019@gmail.com>
2025-03-03 20:37:59 -0500

    subclases

--- propiedades.js
+++ propiedades.js
@@ -15,4 +15,35 @@ if (name) {
 const user = { nombre: `Valentino` };
 console.log(`nombre:` in user);
 
+
+class Persona {
+    constructor(nombre, edad) {
+      this.nombre = nombre;
+      this.edad = edad;
+    }
+  
+    saludar() {
+      console.log(`Hola, mi nombre es ${this.nombre} y tengo ${this.edad} años.`);
+    }
+  }
+  
+  // Clase hija (subclase)
+  class Estudiante extends Persona {
+    constructor(nombre, edad, carrera) {
+      super(nombre, edad); // Llama al constructor de la superclase
+      this.carrera = carrera;
+    }
+  
+    presentarse() {
+      this.saludar();
+      console.log(`Estudio ${this.carrera}.`);
+    }
+  }
+  
+  // Instancia de la subclase
+  const estudiante = new Estudiante('Valentino', 20, 'Ingeniería');
+  estudiante.presentarse();
+
 //link: https://builtin.com/articles/javascript-check-if-object-has-property
+
+//link: https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Classes
