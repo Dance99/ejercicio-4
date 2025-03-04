0a89cbc9c1b71fc4e2cee9bbaa0dd3434e05067b
Daniela <nam991019@gmail.com>
2025-03-03 19:40:59 -0500

    recorrido array mayusculas

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 // Recorrer un array
 
-const num = 
+const num = [`gasas`, `torundas`,` vendas`, `tensiometro`, `monitor` ];
+
+//bucle con ciclo for
+// for (i = 0; i < num.length; i++){
+//     console.log(num[i]);
+// }
+
+//El bucle forEach funcion callback( que s para cambiar de una funcion a otra)
+
+num.forEach((num, index) => {
+    console.log(`Índice: ${index}, Valor: ${num.toUpperCase()}`);
+});
+//esta funcion ejecutara cada elemento
+
