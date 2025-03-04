402fdfffaa6bfb9d8c13ed15f5b8b9f2d47f87a0
Daniela <nam991019@gmail.com>
2025-03-03 19:49:45 -0500

    forEach numeros

---
+++ salida.js
@@ -0,0 +1,6 @@
+const number = [ 10,20,30,40];
+
+//ciclo forEach
+number.forEach((number, index) => {
+    console.log(`index:  `)
+})
--- script.js
+++ script.js
@@ -10,7 +10,8 @@ const num = [`gasas`, `torundas`,` vendas`, `tensiometro`, `monitor` ];
 //El bucle forEach funcion callback( que s para cambiar de una funcion a otra)
 
 num.forEach((num, index) => {
-    console.log(`Índice: ${index}, Valor: ${num.toUpperCase()}`);
+    console.log(`Índice: ${index}, Valor: ${num.toUpperCase()}`); 
+    //El toUpperCase() método devuelve el valor convertido en mayúsculas de la cadena que realiza la llamada.
 });
 //esta funcion ejecutara cada elemento
 
